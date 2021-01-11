8adc8d89784e86826778522f5ef87b67f839f600
Nicholas <nicholas.navarro@edu.turkuamk.fi>
2021-01-11 11:46:22 +0200

    Completed Coding Exercise 1 File

---
+++ Exercise1/HelloWorld.c
@@ -0,0 +1,14 @@
+/*
+File: HelloWorld.c
+Author: Nicholas Navarro
+Description: Printing HelloWorld! to console.
+*/
+
+#include <stdio.h> //including the studio.h pre-process to be able to use printf
+//main function is always called on first, and we define our print function here
+int main () {
+
+  printf("Hello World!\n"); // printing text with a new line call /n
+  return 0; // returning 0 to our main function so the program can stop
+
+}
