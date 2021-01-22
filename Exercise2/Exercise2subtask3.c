ad1cb19741d9c5b56c16ddc58153a32d92e0d036
nicknav98 <nicholasnavarro98@gmail.com>
2021-01-22 13:15:11 +0200

    updating code with proper comments and making source code more readable

--- Exercise2/Exercise2subtask3.c
+++ Exercise2/Exercise2subtask3.c
@@ -9,20 +9,20 @@ Description: Completing 3 Tasks set out in Exercise 2 of course.
 int main() {
   /* code */
 
-int firstNumber = 0;
-int secondNumber = 0;
-int temp = 0;
-int status = 0;
+int firstNumber = 0; //user inputed first number
+int secondNumber = 0; //user inputed second number
+int temp = 0; // temp variable to catch ExceptionHandling
+int status = 0; // status variable for while loop when validating user input
 
 
 printf("Enter an Integer: ");
-status = scanf("%d", &firstNumber);
+status = scanf("%d", &firstNumber); //scanning user input and assigning 1 to status to allow while loop to run
 while(status!=1){
-    while((temp=getchar()) != EOF && temp != '\n');
+    while((temp=getchar()) != EOF && temp != '\n'); //temp var holding character of input
     printf("Invalid input...");
     return 0;
  }
-
+//repeated code for second user input
 printf("Enter another Integer: ");
 status = scanf("%d", &secondNumber);
 while(status!=1){
@@ -31,11 +31,11 @@ while(status!=1){
     return 0;
  }
 
-if(firstNumber>secondNumber)
+if(firstNumber>secondNumber) //comparrison using >
   printf("Your first Number : %d is bigger than your second number: %d !\n", firstNumber, secondNumber);
-if(secondNumber>firstNumber)
+if(secondNumber>firstNumber) //integer size variable
   printf("Your second number: %d is bigger than your first number: %d !\n", secondNumber, firstNumber);
-if(firstNumber==secondNumber)
+if(firstNumber==secondNumber) //check if both integers are equal
   printf("Both numbers are equal: %d and %d!\n", firstNumber, secondNumber);
 
 
