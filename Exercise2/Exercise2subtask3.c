3ebac91d43a672433f7b36d8d098997779f5c0e0
Nicholas <nicholas.navarro@edu.turkuamk.fi>
2021-01-22 15:31:13 +0200

    further development of exercises, subtask3 complete,testing to follow

--- Exercise2/Exercise2subtask3.c
+++ Exercise2/Exercise2subtask3.c
@@ -5,6 +5,7 @@ Description: Completing 3 Tasks set out in Exercise 2 of course.
 */
 
 #include <stdio.h>
+#include <math.h>
 
 int main() {
   /* code */
@@ -13,6 +14,11 @@ int firstNumber = 0; //user inputed first number
 int secondNumber = 0; //user inputed second number
 int temp = 0; // temp variable to catch ExceptionHandling
 int status = 0; // status variable for while loop when validating user input
+int sumNumbers = 0; // var to hold sum
+float avgNumbers = 0; //var to hold average of two numbers
+int diffNumbers = 0; //var to hold difference between 2 numbers
+float divNumbers = 0; //var to hold division of 2 numbers
+int powerNumbers = 0; //var to hold 1st to the power of 2nd number
 
 
 printf("Enter an Integer: ");
@@ -37,6 +43,29 @@ if(secondNumber>firstNumber) //integer size variable
   printf("Your second number: %d is bigger than your first number: %d !\n", secondNumber, firstNumber);
 if(firstNumber==secondNumber) //check if both integers are equal
   printf("Both numbers are equal: %d and %d!\n", firstNumber, secondNumber);
+
+int sumNumbers = firstNumber + secondNumber;
+printf("The sum of these two integers are: %d", sumNumbers);
+
+float avgNumbers = sumNumbers/2;
+printf("The average of these two integers are: %d", avgNumbers)
+
+if(secondNumber = 0){
+  printf("Division function cannot be executed due to a zero divisor");
+}
+else{
+  float divNumbers = firstNumber/secondNumber;
+  printf("The first number divided by the second number is : %d", divNumbers);
+}
+
+int powerNumbers = pow(firstNumber,secondNumber);
+
+printf("The first number to the power of the second number is: ", powerNumbers);
+
+
+
+
+
 
 
 
