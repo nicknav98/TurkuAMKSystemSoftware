0fff359ebcf9f712bfa0e807dbfb008a859fbc13
nicknav98 <nicholasnavarro98@gmail.com>
2021-02-01 15:08:13 +0200

    subtask 4 in progress, basic code structure in progress.

--- Exercise3/Exercise3FibSeq.c
+++ Exercise3/Exercise3FibSeq.c
@@ -8,9 +8,16 @@ Description:
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include
 
-void isPerfectSquare(test){}
+void isPerfectSquare(int test){
+  int s = (int) math.sqrt(test);
+  return (s*s == test);
+}
 
+void isFibonacci(int n){
+  return isPerfectSquare(5*n*n + 4) || isPerfectSquare(5*n*n-4);
+}
 
 
 
@@ -19,14 +26,21 @@ void isPerfectSquare(test){}
 
 
 int main(){
-fiboRandomNUm = 0;
+int fiboRandomNUm = 0;
 
-unsigned seed = time(0)
-srand(seed)
-fiboRandomNUm = rand() % 1000 //test range
+unsigned seed = time(0);
+srand(seed);
+fiboRandomNUm = rand() % 1000; //test range
+
+printf("%d is the random number.", fiboRandomNUm);
+
+if (isFibonacci(fiboRandomNUm) == True){
+  printf("%d is a Fibonacci Number!", fiboRandomNUm);
+}else{
+  printf("%d is not a Fibonacci Number.", fiboRandomNUm);
+}
 
 
-
-
+return 0;
 
 }
